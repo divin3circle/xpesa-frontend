b357da78c8074d1ba0e7143b571ad439a768008e
divin3circle <sylus.abel@icloud.com>
2026-04-10 14:34:19 +0300

    fix: suspense related error in error page preventing build

--- app/error/page.tsx
+++ app/error/page.tsx
@@ -1,35 +1,16 @@
-"use client"
-import { Button } from "@/components/ui/button"
-import { onNavigate } from "@/lib/utils"
-import { ReloadIcon } from "hugeicons-react"
-import Image from "next/image"
-import { useRouter, useSearchParams } from "next/navigation"
+import { Suspense } from "react"
+import ClientError from "@/components/error/client-error"
 
-function ErrorPage() {
-  const router = useRouter()
-  const errorMessage =
-    useSearchParams().get("message") || "An unexpected error occurred."
+function ErrorPage({
+  searchParams,
+}: {
+  searchParams: Promise<{ q?: string }>
+}) {
   return (
-    <div className="flex h-screen w-full flex-col items-center justify-center">
-      <p className="text-lg font-medium">Whoops! That didn&apos;t work.</p>
-      <Image
-        src="/error.png"
-        alt="Error"
-        width={300}
-        height={200}
-        className="my-6"
-      />
-      <p className="font-sans font-semibold text-destructive">Reason:</p>
-      <p className="font-sans text-muted-foreground">{errorMessage}</p>
-      <Button
-        variant="outline"
-        className="mt-4 w-full md:w-1/3"
-        onClick={() => onNavigate("/login", router)}
-      >
-        <ReloadIcon className="mr-2" />
-        Try Again
-      </Button>
-    </div>
+    <Suspense fallback={<>....</>}>
+      <ClientError searchParams={searchParams} />
+    </Suspense>
+
   )
 }
 
---
+++ components/error/client-error.tsx
@@ -0,0 +1,38 @@
+'use client'
+
+import { use } from 'react'
+import { Button } from "../ui/button"
+import Image from "next/image"
+import { ReloadIcon } from "hugeicons-react"
+import { onNavigate } from "@/lib/utils"
+import { useRouter } from "next/navigation";
+
+export default function ClientError({ searchParams }: {
+  searchParams: Promise<{ q?: string }>
+}) {
+  const router = useRouter()
+  const errorMessage = use(searchParams)
+  return (
+    <div className="flex h-screen w-full flex-col items-center justify-center">
+      <p className="text-lg font-medium">Whoops! That didn&apos;t work.</p>
+      <Image
+        src="/error.png"
+        alt="Error"
+        width={300}
+        height={200}
+        className="my-6"
+      />
+      <p className="font-sans font-semibold text-destructive">Reason:</p>
+      <p className="font-sans text-muted-foreground">{errorMessage.q}</p>
+      <Button
+        variant="outline"
+        className="mt-4 w-full md:w-1/3"
+        onClick={() => onNavigate("/login", router)}
+      >
+        <ReloadIcon className="mr-2" />
+        Try Again
+      </Button>
+    </div>
+
+  )
+}
