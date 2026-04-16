1206c997cd6de8b66d0e5148c272f6fc67a4af86
divin3circle <sylus.abel@icloud.com>
2026-04-16 20:01:23 +0300

    feat: uncomment and implement getAllTimeEarnings function in use-stats hook

--- hooks/use-stats.ts
+++ hooks/use-stats.ts
@@ -1,29 +1,41 @@
-// import { createClient } from "@/lib/supabase/client"
-// import { useQuery } from "@tanstack/react-query"
+import { createClient } from "@/lib/supabase/client"
+import { useQuery } from "@tanstack/react-query"
 
 // We need stats about:
 // All time earnings, current month earnings, total transactions on creators links, active links.
 
 // const supabase = createClient();
 
-// async function getAllTimeEarnings(): Promise<string> {
-//   try {
-//     const { data: { user } } = await supabase.auth.getUser()
-//     if (!user) return "0"
-//     const { data, error } = await supabase
-//       .from('transactions')
-//       .select('total_earned_usdc')
-//       .eq('creator_id', user.id)
+async function getAllTimeEarnings(): Promise<string> {
+  try {
+    const supabase = createClient()
+    const {
+      data: { user },
+    } = await supabase.auth.getUser()
+    if (!user) return "0"
+    const { data, error } = await supabase
+      .from("transactions")
+      .select("total_earned_usdc")
+      .eq("creator_id", user.id)
 
-//     console.log("Error getting all time earnings: ", error)
-//     const sum = data?.reduce((a, c) => Number(a.total_earned_usdc) + Number(c.total_earned_usdc), 0);
+    console.log("Error getting all time earnings: ", error)
+    const sum = data?.reduce((a, c) => a + Number(c.total_earned_usdc), 0)
 
-//     return sum?.toLocaleString(undefined, {
-//       maximumFractionDigits: 2,
-//       minimumFractionDigits: 2
-//     }) || "0"
-//   } catch (error) {
-//     console.log("Error getting all time earnings: ", error);
-//     return "0"
-//   }
-// }
+    return (
+      sum?.toLocaleString(undefined, {
+        maximumFractionDigits: 2,
+        minimumFractionDigits: 2,
+      }) || "0"
+    )
+  } catch (error) {
+    console.log("Error getting all time earnings: ", error)
+    return "0"
+  }
+}
+
+export function useGetAllTimeEarnings() {
+  return useQuery({
+    queryKey: ["allTimeEarnings"],
+    queryFn: getAllTimeEarnings,
+  })
+}
