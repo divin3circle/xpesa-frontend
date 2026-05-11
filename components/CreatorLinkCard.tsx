00c7f562eb45e864a386bb2640b814da036329d9
divin3circle <sylus.abel@icloud.com>
2026-05-11 15:17:34 +0300

    fix: extract links views into own component in creator view

---
+++ components/CreatorLinkCard.tsx
@@ -0,0 +1,70 @@
+import { LinkPublic } from "@/app/api/public/creator/[handle]/route"
+import Image from "next/image"
+import { getLinkImageURL } from "@/lib/utils"
+import { Button } from "@/components/ui/button"
+import { ArrowRight01Icon } from "hugeicons-react"
+import { Badge } from "@/components/ui/badge"
+import { Card, CardContent } from "@/components/ui/card"
+import { useRouter } from "next/navigation"
+import { useTransactionsByLinkId } from "@/hooks/use-transactions"
+import LoadingSpinner from "@/components/ui/loading-spinner"
+
+function formatUSDC(value: number | null) {
+  if (!value || value <= 0) return "Free"
+
+  return `$${value.toLocaleString(undefined, {
+    minimumFractionDigits: 2,
+    maximumFractionDigits: 2,
+  })}`
+}
+
+function formatType(type: string) {
+  return type.charAt(0).toUpperCase() + type.slice(1)
+}
+
+export default function CreatorLinkCard({ link}: {
+  link: LinkPublic
+}) {
+  const { data, isLoading } = useTransactionsByLinkId(link.id, 10)
+  const router = useRouter()
+  return (
+    <Card key={link.id} className="overflow-hidden border-border/70">
+      <div className="relative h-56 w-full overflow-hidden">
+        <Image
+          src={getLinkImageURL(link.type)}
+          alt={link.title}
+          fill
+          className="object-cover"
+        />
+      </div>
+
+      <CardContent className="space-y-2 p-4">
+        <p className="line-clamp-2 truncate font-medium">{link.title}</p>
+
+        <div className="flex items-center gap-2">
+          <Badge variant="outline">{formatType(link.type)}</Badge>
+          <Badge variant="secondary">{formatUSDC(link.price_usdc)}</Badge>
+        </div>
+
+        <div className="flex items-center justify-between text-xs text-muted-foreground">
+          <span>{(link.view_count ?? 0).toLocaleString()} views</span>
+          {isLoading ? (
+            <LoadingSpinner />
+          ) : (
+            <span>
+              {(data?.transactions.length ?? 0).toLocaleString()} sales
+            </span>
+          )}
+        </div>
+        <Button
+          variant="ghost"
+          className="group mt-4 w-full"
+          onClick={() => router.push(`/pay/${link.id}`)}
+        >
+          See details
+          <ArrowRight01Icon className="size-4 duration-500 ease-in-out group-hover:translate-x-3.5" />
+        </Button>
+      </CardContent>
+    </Card>
+  )
+}
--- components/creator-public/creator-links-grid.tsx
+++ components/creator-public/creator-links-grid.tsx
@@ -1,39 +1,23 @@
 "use client"
 
-import Image from "next/image"
+
 import { useMemo, useState } from "react"
 import { Search } from "lucide-react"
 
 import type { LinkPublic } from "@/app/api/public/creator/[handle]/route"
-import { Badge } from "@/components/ui/badge"
 import { Card, CardContent } from "@/components/ui/card"
 import { Input } from "@/components/ui/input"
-import { getLinkImageURL } from "@/lib/utils"
-import { Button } from "../ui/button"
-import { ArrowRight01Icon } from "hugeicons-react"
-import { useRouter } from "next/navigation"
+import CreatorLinkCard from "../CreatorLinkCard"
 
 type CreatorLinksGridProps = {
   links: LinkPublic[]
 }
 
-function formatUsdc(value: number | null) {
-  if (!value || value <= 0) return "Free"
 
-  return `$${value.toLocaleString(undefined, {
-    minimumFractionDigits: 2,
-    maximumFractionDigits: 2,
-  })}`
-}
-
-function formatType(type: string) {
-  return type.charAt(0).toUpperCase() + type.slice(1)
-}
 
 export function CreatorLinksGrid({ links }: CreatorLinksGridProps) {
   const [searchTerm, setSearchTerm] = useState("")
 
-  const router = useRouter()
   const filteredLinks = useMemo(() => {
     const normalized = searchTerm.trim().toLowerCase()
     if (!normalized) return links
@@ -77,44 +61,7 @@ export function CreatorLinksGrid({ links }: CreatorLinksGridProps) {
       ) : (
         <div className="grid gap-4 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4">
           {filteredLinks.map((link) => (
-            <Card key={link.id} className="overflow-hidden border-border/70">
-              <div className="relative h-56 w-full overflow-hidden">
-                <Image
-                  src={getLinkImageURL(link.type)}
-                  alt={link.title}
-                  fill
-                  className="object-cover"
-                />
-              </div>
-
-              <CardContent className="space-y-2 p-4">
-                <p className="line-clamp-2 truncate font-medium">
-                  {link.title}
-                </p>
-
-                <div className="flex items-center gap-2">
-                  <Badge variant="outline">{formatType(link.type)}</Badge>
-                  <Badge variant="secondary">
-                    {formatUsdc(link.price_usdc)}
-                  </Badge>
-                </div>
-
-                <div className="flex items-center justify-between text-xs text-muted-foreground">
-                  <span>{(link.view_count ?? 0).toLocaleString()} views</span>
-                  <span>
-                    {(link.payment_count ?? 0).toLocaleString()} sales
-                  </span>
-                </div>
-                <Button
-                  variant="ghost"
-                  className="group mt-4 w-full"
-                  onClick={() => router.push(`/pay/${link.id}`)}
-                >
-                  See details
-                  <ArrowRight01Icon className="size-4 duration-500 ease-in-out group-hover:translate-x-3.5" />
-                </Button>
-              </CardContent>
-            </Card>
+            <CreatorLinkCard link={link} key={link.id} />
           ))}
         </div>
       )}
