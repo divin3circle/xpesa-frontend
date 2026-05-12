ed92f73a70305be65b0a2c6f8e0654c8ed4ef5e9
divin3circle <sylus.abel@icloud.com>
2026-05-12 22:17:03 +0300

    fix: redirect auth bug

--- hooks/use-auth.ts
+++ hooks/use-auth.ts
@@ -15,25 +15,16 @@ interface AuthResponse {
 
 export type AvailableAuthProviders = 'twitter' | 'google';
 
-function getAuthRedirectUrl(next = "/auth/post-login") {
-  const origin =
-    typeof window !== "undefined" ? window.location.origin : envConfig.APP_URL
-
-  const url = new URL("/auth/callback", origin)
-  url.searchParams.set("next", next)
-
-  return url.toString()
-}
-
 async function signInWithAProvider({ provider }: {
   provider: AvailableAuthProviders
 }) {
   try {
     const supabase = createClient()
+    console.log(envConfig.APP_URL + "/auth/post-login")
     const { data, error } = await  supabase.auth.signInWithOAuth({
       provider,
       options: {
-        redirectTo: getAuthRedirectUrl("/auth/post-login")
+        redirectTo: envConfig.APP_URL + "/auth/post-login",
       }
     })
     if(error){
